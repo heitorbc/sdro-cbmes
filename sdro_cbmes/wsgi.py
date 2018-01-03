570816390afa268b3becd9801a2cf865b467851e
Heitor BC <heitorhbc@gmail.com>
2018-01-03 20:14:33 +0000

    acerto wgsi whitenoise

--- sdro_cbmes/wsgi.py
+++ sdro_cbmes/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+##comentar/descomentar
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sdro_cbmes.settings")
 
 application = get_wsgi_application()
+##comentar/descomentar
+application = DjangoWhiteNoise(application)
